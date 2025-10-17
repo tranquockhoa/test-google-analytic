7b4f59d452c61894a8691e1e87bb260d060c6094
tranquockhoa <khoatran1.tt.nd@gmail.com>
2025-10-17 17:43:45 +0700

    feat: update

--- app/ultils/gtag.js
+++ app/ultils/gtag.js
@@ -1,5 +1,6 @@
-export const event = ({ action, value }) => {
+export const event = (action, value) => {
   if (window.gtag !== "undefined") {
     window.gtag("event", action, value);
+    console.log("GA event sent:", action, value);
   }
 };
